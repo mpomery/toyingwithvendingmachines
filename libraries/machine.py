8e9c579810aa347eef3fdb4996df557d2bd622df
Mitchell Pomery <bob_george33@hotmail.com>
2015-04-18 11:50:24 +0800

    comments

--- libraries/machine.py
+++ libraries/machine.py
@@ -2,6 +2,10 @@ import socket
 from time import sleep
 from VendingMachine import VendingMachine
 
+
+"""
+Set up the connection to the vending machine. You shouldn't need to edit this code
+"""
 def connect_to_vendingmachine():
 	print("Connecting to Virtual Vending Machine")
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
@@ -11,10 +15,17 @@ def connect_to_vendingmachine():
 	print("Connected")
 	return rfh, wfh
 
+"""
+This is the main part of the program
+"""
 def main():
+	# Connect to the vending machine
 	rfh, wfh = connect_to_vendingmachine()
 	vm = VendingMachine(rfh, wfh)
+
+	# Anything you want to do prior to starting your program should happen here
 	while True:
+		# Everything you want to run continuously should go here
 		for i in ["/LOADING\\", "-LOADING-", "\\LOADING/", "|LOADING|"]:
 			vm.display(i)
 			sleep(0.5)
